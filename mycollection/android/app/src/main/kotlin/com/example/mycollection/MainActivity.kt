476371927b4d4d10c864a60210ff3890be6a697e
herry88 <herry081288@gmail.com>
2021-02-26 17:31:54 +0700

    MyFlutterUIChallenges

---
+++ mycollection/android/app/src/main/kotlin/com/example/mycollection/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mycollection
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mycollection/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Ui Collection',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.green,
+            expandedHeight: 200.0,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text('Challenges'),
+            
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
