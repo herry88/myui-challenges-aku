68d45a3b940f31e983fabb0bff1425674340eebd
herry88 <herry081288@gmail.com>
2021-03-31 14:37:02 +0700

    AddFoodApps

---
+++ mycollection/lib/foodApps/foodAppMaster.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class FoodApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text(
+          'Test',
+        ),
+      ),
+      body: Center(
+        child: new Text(
+          'coba',
+        ),
+      ),
+    );
+  }
+}
