e8d3d4758a1cf324c55baab3fddeec383c932b47
herry88 <herry081288@gmail.com>
2021-04-02 10:33:25 +0700

    AddCourserPageHome

--- mycollection/lib/onlinecourse/ui/coursehomepage.dart
+++ mycollection/lib/onlinecourse/ui/coursehomepage.dart
@@ -11,22 +11,167 @@ class _CourseHomePageState extends State<CourseHomePage> {
     return ListView(
       children: [
         Padding(
-            padding: const EdgeInsets.only(
-              left: 16.0,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Class Preview',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
+          padding: const EdgeInsets.only(
+            left: 16.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Class Preview',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 16.0,
+                ),
+                child: Container(
+                  height: 230.0,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          new Container(
+                            height: 160,
+                            width: 240,
+                            decoration: BoxDecoration(
+                              color: Colors.redAccent,
+                              borderRadius: BorderRadius.circular(
+                                8.0,
+                              ),
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                  "https://cdn.pixabay.com/photo/2015/01/08/18/24/children-593313_960_720.jpg",
+                                ),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 16.0,
+                          ),
+                          Text(
+                            'Kelas A',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8.0),
+                            child: Container(
+                              height: 1.0,
+                              width: 100.0,
+                              decoration: BoxDecoration(
+                                color: Colors.grey,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            'Flutter Iseng UI',
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 16.0,
+                        ),
+                        child: Column(
+                          children: [
+                            Container(
+                              height: 160.0,
+                              width: 240.0,
+                              decoration: BoxDecoration(
+                                color: Colors.redAccent,
+                                borderRadius: BorderRadius.circular(
+                                  8.0,
+                                ),
+                                image: DecorationImage(
+                                  image: NetworkImage(
+                                    'https://cdn.pixabay.com/photo/2015/01/08/18/24/children-593313_960_720.jpg',
+                                  ),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 16.0,
+                            ),
+                            Text(
+                              'Kelas B',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 8.0,
+                              ),
+                              child: Container(
+                                height: 1.0,
+                                width: 100.0,
+                                decoration: BoxDecoration(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              'Iseng ',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            )),
+              ),
+              Text(
+                'Popular Classes',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 16.0,
+                ),
+                child: ListView.separated(
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(
+                          right: 24.0,
+                        ),
+                        child: Container(
+                          height:20.0,
+                          child:Row(
+                            children: [
+                              Expanded(child: Padding(padding: const EdgeInsets.all(8.0),),),
+
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                    separatorBuilder: separatorBuilder,
+                    itemCount: itemCount),
+              )
+            ],
+          ),
+        ),
       ],
     );
   }
